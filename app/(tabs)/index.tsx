efb8dec238eac7f9adbce9327f0692215d33a762
Jacob Oursland <jacob.oursland@gmail.com>
2023-09-04 13:35:36 -0700

    Add suggested changes from https://docs.expo.dev/router/reference/authentication/

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,16 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
+import { useAuth } from '../../context/auth';
+
 export default function TabOneScreen() {
+  const { signOut } = useAuth();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text onPress={() => signOut()}>Sign Out</Text>
     </View>
   );
 }
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -3,12 +3,16 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
+import { useAuth } from '../../context/auth';
+
 export default function TabTwoScreen() {
+  const { signOut } = useAuth();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Text onPress={() => signOut()}>Sign Out</Text>
     </View>
   );
 }
